1d77157005705085bb1624ff297a551e069dbbc1
Karan <karandeepbatra@gmail.com>
2019-09-21 11:55:02 +0530

    Add Singleton

---
+++ src/com/patterns/Singleton/Singleton.java
@@ -0,0 +1,13 @@
+package com.patterns.Singleton;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null)
+            uniqueInstance = new Singleton();
+        return uniqueInstance;
+    }
+}
